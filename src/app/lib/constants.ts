448fb91d29ebe3f68c8130df7c96aa6e99f74c67
xxxZino <egasaputraaa12@students.amikom.ac.id>
2026-03-16 00:27:48 +0700

    ganti link

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -15,20 +15,20 @@ export const WORKS: Work[] = [
     title: "Project Internship 1",
     tag: "bahaya judi online",
     cover:"/internship.jpg",
-    href:"https://drive.google.com/file/d/1zMOdepiZuI2yYtW35LD9GVxkqaV3PPk0/view?usp=drive_link",
+    href:"https://drive.google.com/drive/folders/13bqTifwWee9CaWJ8XnD33nYUxs-CixZO",
   },
   {
     title: "Project Internship 2",
     tag: "micro skill implementasi pancasila",
     cover:"/internship.jpg",
-    href: "https://drive.google.com/file/d/1AI2kcgxvJwqaDVa2tQ7LmdBrgYsEbMdp/view?usp=drive_link",
+    href: "https://drive.google.com/drive/folders/13bqTifwWee9CaWJ8XnD33nYUxs-CixZO",
   },
 
   {
     title: "Project Internship 3",
     tag: "smart village",
     cover:"/internship.jpg",
-    href: "https://drive.google.com/file/d/1dd7kAN9b3QC-QlARwXih60TzPKU8RX4I/view?usp=drive_link" ,
+    href: "https://drive.google.com/drive/folders/13bqTifwWee9CaWJ8XnD33nYUxs-CixZO" ,
   },
   {
     title: "Cinematic",
